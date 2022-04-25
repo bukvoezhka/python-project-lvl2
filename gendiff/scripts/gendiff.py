a4662a64d693845d98439d8870efa1d37ea649ef
Oleg Izyumov <bukvoezhka@gmail.com>
2022-04-25 22:46:49 +0300

    Prepare repo to project (step #1)

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+"""Gendiff module description."""
+
+import argparse
+
+
+def main():
+    """Output description for CLI."""
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+
+if __name__ == '__main__':
+    main()
