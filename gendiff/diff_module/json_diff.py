b0978e5b37c90a5f893af6952f279fd1d5aef9ec
Oleg Izyumov <bukvoezhka@gmail.com>
2022-05-09 14:25:05 +0300

    Refactor for codeclimate maintainability

--- gendiff/diff_module/json_diff.py
+++ gendiff/diff_module/json_diff.py
@@ -18,22 +18,23 @@ def generate_diff(json_file1, json_file2):
     json_dict1, json_dict2 = convert_json_to_dict(json_file1, json_file2)
     for key in sorted(set(json_dict1) | (set(json_dict2))):
         key_diff_status = check_key_statement(key, json_dict1, json_dict2)
-        if key_diff_status == 'both':
-            if json_dict1.get(key) == json_dict2.get(key):
-                JSON_DIFF_DICT.update({
-                    ('  {0}').format(key): json_dict1.get(key),
-                })
-            else:
-                JSON_DIFF_DICT.update({
-                    ('- {0}').format(key): json_dict1.get(key),
-                    ('+ {0}').format(key): json_dict2.get(key),
-                })
         if key_diff_status == 'first':
             JSON_DIFF_DICT.update({
                 ('- {0}').format(key): json_dict1.get(key),
             })
+            continue
         if key_diff_status == 'second':
             JSON_DIFF_DICT.update({
+                ('+ {0}').format(key): json_dict2.get(key),
+            })
+            continue
+        if json_dict1.get(key) == json_dict2.get(key):
+            JSON_DIFF_DICT.update({
+                ('  {0}').format(key): json_dict1.get(key),
+            })
+        else:
+            JSON_DIFF_DICT.update({
+                ('- {0}').format(key): json_dict1.get(key),
                 ('+ {0}').format(key): json_dict2.get(key),
             })
     return json.dumps(JSON_DIFF_DICT, indent=2)
@@ -51,8 +52,6 @@ def check_key_statement(key, dict1, dict2):
     Returns:
         state of key in dict.
     """
-    if key in dict1 and key in dict2:
-        return 'both'
     if key in dict1 and key not in dict2:
         return 'first'
     if key not in dict1 and key in dict2:
