28bc7b1f7f4a0c393813ece58529e71282756af7
Oleg Izyumov <bukvoezhka@gmail.com>
2022-09-23 20:15:49 +0300

    Add tests for nested files and some housekeeping

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -4,14 +4,21 @@ from gendiff.diff_module.generate_diff import generate_diff
 
 FIXTURES_REPO = Path('tests', 'fixtures')
 
-FIRST_FLAT_JSON = Path(FIXTURES_REPO, 'first_flat_file.json')
-SECOND_FLAT_JSON = Path(FIXTURES_REPO, 'second_flat_file.json')
+FIRST_FLAT_JSON = Path(FIXTURES_REPO, 'flat_file1.json')
+SECOND_FLAT_JSON = Path(FIXTURES_REPO, 'flat_file2.json')
+FIRST_NESTED_JSON = Path(FIXTURES_REPO, 'nested_file1.json')
+SECOND_NESTED_JSON = Path(FIXTURES_REPO, 'nested_file2.json')
 
-FIRST_FLAT_YAML = Path(FIXTURES_REPO, 'first_flat_file.yml')
-SECOND_FLAT_YAML = Path(FIXTURES_REPO, 'second_flat_file.yml')
+FIRST_FLAT_YAML = Path(FIXTURES_REPO, 'flat_file1.yml')
+SECOND_FLAT_YAML = Path(FIXTURES_REPO, 'flat_file2.yml')
+FIRST_NESTED_YAML = Path(FIXTURES_REPO, 'nested_file1.yml')
+SECOND_NESTED_YAML = Path(FIXTURES_REPO, 'nested_file2.yml')
 
 COMPARE_FLAT_FILES = Path(
     FIXTURES_REPO, 'compare_flat_files.txt',
+).read_text().replace(r'\n', '\n')
+COMPARE_NESTED_FILES = Path(
+    FIXTURES_REPO, 'compare_nested_files.txt',
 ).read_text().replace(r'\n', '\n')
 
 
@@ -21,6 +28,10 @@ def test_generate_json_diff():
         FIRST_FLAT_JSON,
         SECOND_FLAT_JSON,
     ) == COMPARE_FLAT_FILES
+    assert generate_diff(
+        FIRST_NESTED_JSON,
+        SECOND_NESTED_JSON,
+    ) == COMPARE_NESTED_FILES
 
 
 def test_generate_yaml_diff():
@@ -29,6 +40,10 @@ def test_generate_yaml_diff():
         FIRST_FLAT_YAML,
         SECOND_FLAT_YAML,
     ) == COMPARE_FLAT_FILES
+    assert generate_diff(
+        FIRST_NESTED_YAML,
+        SECOND_NESTED_YAML,
+    ) == COMPARE_NESTED_FILES
 
 
 def test_files_extension():
