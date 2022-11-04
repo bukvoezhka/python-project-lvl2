7ad91fa777ab5f847fd298118610694fd0a66eb8
Oleg Izyumov <bukvoezhka@gmail.com>
2022-11-04 21:15:47 +0300

    Update for support format choise

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,7 +3,13 @@
 import argparse
 
 from gendiff.engine import generate_diff
-from gendiff.stylish import pretty_print_ast
+from gendiff.formatters.plain import make_ast_plain_view
+from gendiff.formatters.stylish import make_ast_tree_view
+
+FORMATTERS = {
+    None: make_ast_tree_view,
+    'plain': make_ast_plain_view,
+}
 
 
 def main():
@@ -15,11 +21,14 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(generate_diff(
-        first_file=args.first_file,
-        second_file=args.second_file,
-        formatter=pretty_print_ast,
-    ))
+    try:
+        print(generate_diff(
+            first_file=args.first_file,
+            second_file=args.second_file,
+            formatter=FORMATTERS[args.format],
+        ))
+    except KeyError:
+        print('The specified format is invalid.')
 
 
 if __name__ == '__main__':
